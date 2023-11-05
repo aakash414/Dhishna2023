cd978ab00dc8d08d5905422a6f5bd2184896ae9c
aakash <aakashpr975@gmail.com>
2023-11-05 16:57:20 +0530

    events slide

--- src/components/Slide3.jsx
+++ src/components/Slide3.jsx
@@ -4,13 +4,11 @@ export function Slide3() {
   return (
     <div className="mt-[400px] flex h-[100vh] flex-col">
       <div className="h-full">
-        <h1 className="font-thunder font-500  h-full w-full text-[600px] italic text-white">
+        <h1 className="font-500 min-tez  h-full w-full justify-center font-thunder italic text-white">
           Events
-          <div className="circle orange"></div>
-          <div className="circle purple"></div>
         </h1>
       </div>
-      <div className="font-satoshi font-200 flex flex-row justify-around  text-white">
+      <div className="font-200 flex flex-row justify-around font-satoshi  text-white">
         <div>
           <p>CHAKRA</p>
         </div>
@@ -23,11 +21,6 @@ export function Slide3() {
         <div>
           <p>ROCKET</p>
         </div>
-      </div>
-      <div className="min-h-screen bg-gray-900 flex items-center justify-center">
-        <h1 className="text-white text-center font-bold text-5xl animate-shine bg-clip-text text-transparent bg-gradient-to-r from-white via-yellow-400 to-white">
-          Shine!
-        </h1>
       </div>
     </div>
   )
