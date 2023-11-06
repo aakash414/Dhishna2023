bbe887add1240fdcc3dd52e10fe7c81c5c8dd5d4
dhanush <dhanushpk50@gmail.com>
2023-11-06 21:12:49 +0530

    minor changes

--- src/components/Slide1.jsx
+++ src/components/Slide1.jsx
@@ -3,20 +3,20 @@ import React from 'react'
 export function Slide1() {
   return (
     <div className='relative text-white h-[100vh]'>
-              <div class="circle"></div>
-        <div class="circle ad13b2  "></div>
+              <div className="circle"></div>
+        <div className="circle ad13b2  "></div>
       
-      <div className='absolute opacity-60'>
+      <div className='absolute opacity-70'>
         <img src='/Dhishna.png' alt="Dhishna" className='hidden md:block w-[550px] lg:w-[650px] xl:w-[750px] ' />
       </div>
       <div className='absolute flex justify-center items-center h-full w-full opacity-60'>
-        <p className='font-bold test italic bg-clip-text bg-gradient-to-r from-yellow-500 to-red-500 p-8'>20<br />&nbsp;23</p>
+        <p className='font-bold test italic bg-clip-text p-8 font-thunder'>20<br />&nbsp;23</p>
 
       </div>
-      <div className='relative h-full w-full flex items-end p-10'>
-        <div className='flex flex-col w-full gap-2'>
+      <div className='relative h-full w-full flex items-end p-10 font-bold font-satoshi'>
+        <div className='flex flex-col w-full gap-2 font-medium'>
           <p className='text-gray-400'>COCHIN UNIVERSITY OF SCIENCE AND TECHNOLOGY</p>
-          <p>NOV 2023</p>
+          <p className='font-bold'>NOV 2023</p>
           <div className='flex flex-row items-center gap-3 w-full'>
             <p>24</p>
             <div className='h-0.5 w-10 bg-white'></div>
