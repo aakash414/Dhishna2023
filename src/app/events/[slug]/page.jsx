d758583461e0dc5d98881433256b6d28f42754c1
aakash <aakashpr975@gmail.com>
2023-11-09 00:02:22 +0530

    events slug completed

--- src/app/events/[slug]/page.jsx
+++ src/app/events/[slug]/page.jsx
@@ -1,20 +1,30 @@
 "use client"
-import client from '@/client';
+import client from '../../../client';
 import React, { useEffect, useState } from 'react';
-import imageUrlBuilder from '@sanity/image-url';
+import  imageUrlBuilder  from '@sanity/image-url';
+
 
 
 function Page() {
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
 
+    function formatDate(dateString) {
+        const options = { day: 'numeric', month: 'short' };
+        return new Date(dateString).toLocaleDateString(undefined, options);
+    }
+
+    // const builder = imageUrlBuilder(client)
+
+    // function urlFor(source) {
+    //     return builder.image(source);
+    // }
     const builder = imageUrlBuilder(client);
 
     function urlFor(source) {
-        console.log("source",source)
+        // console.log("source",source)
         return builder.image(source);
     }
-
 
 
 
@@ -22,21 +32,32 @@ function Page() {
         // Fetch the data from Sanity when the component mounts
         client
             .fetch(
-                `*[_type == "workshop"]{
+                `*[_type == "events"]{
           title,
           event_code,
           icon,
           details,
           start_date,
           price,
-          venue
+          venue,
+          ticket_end_date,
+          register
         }`
             )
             .then((eventData) => {
+                // console.log("Received data:", eventData);
+                // Filter the data to find the item with a matching event_code
+                const filteredData = eventData.filter((event) => event.event_code == slug);
 
-                const data = eventData.filter((event) => event.event_code == slug);
-                setData(data);
-                setLoading(false);
+                if (filteredData.length > 0) {
+                    setData(filteredData);
+                    // console.log("Received filtered data:", filteredData);
+                    setLoading(false);
+
+                } else {
+                    setLoading(false);
+                    // console.error("Data not found for slug:", slug);
+                }
             })
             .catch((error) => {
                 console.error("Error fetching data:", error);
@@ -46,43 +67,22 @@ function Page() {
 
     const slug = window.location.href.split('/').pop();
     console.log("slug",slug);
+    console.log("data",data)
 
-    console.log("data123",data)
-
-    useEffect(() => {
-        // Fetch data of the event using the slug
-        client
-            .fetch(
-                `*[_type == "workshop" && event_code == "${slug}"]{
-          title,
-          event_code,
-          icon,
-          details,
-          start_date,
-          price,
-          venue
-        }`
-            )
-            .then((data) => {
-                setData(data);
-                console.log("Received filtered data:", data)
-            })
-            .catch(console.error);
-    }, [slug]);
-
-    //test urlFor
-    console.log("data[0].icon",data[0].icon);
 
     return (
-        <div className="py-24 sm:py-32">
-            <div className="px-6 lg:px-8">
+        <div className="py-24 sm:py-32 bg-gray-950">
+            <div className="px-6 mt-8 lg:px-8">
                 {loading ? (
                     <p>Loading...</p>
                 ) : (
                     data && (
-                        <div className="mx-10 flex lg:flex-row flex-col justify-center items-start gap-x-8 gap-y-16 sm:gap-y-24 lg:mx-0 lg:max-w-none">
-                            <div className="overflow-hidden h-fit w-auto rounded-3xl bg-gray-900 shadow-2xl">
-                                <img className="object-cover" src={urlFor(data[0].icon).width(300).height(300).url()} alt="" />
+                        <div className="mx-10 flex lg:flex-row flex-col justify-center items-start gap-x-16 gap-y-14 md:gap-y-14 sm:gap-y-8  lg:mx-0 lg:max-w-none ">
+                            <div className="overflow-hidden h-auto w-auto rounded-3xl bg-gray-900 shadow-2xl">
+                                {/*<img className="object-cover" src={urlFor(data[0].icon).width(500).height(300).url()} alt="" />*/}
+
+                                <img src={urlFor(data[0].icon).width(720).height(720).url()} className={"object-cover object-center  group-hover:scale-90 transition duration-300 ease-in-out"}/>
+
                             </div>
                             <div>
                                 <div className="text-base leading-7 text-gray-300 lg:max-w-lg">
@@ -96,30 +96,35 @@ function Page() {
                                         <p className="mt-6">{data[0].details}</p>
                                     </div>
                                 </div>
-                                <dl className="mt-8 grid grid-cols-2 gap-8 border-t border-gray-300/10 pt-10 sm:grid-cols-4 font-satoshi">
-                                    <div>
-                                        <dt className="text-sm font-semibold leading-6 text-gray-300">Last Date</dt>
-                                        <dd className="mt-2 text-3xl font-bold leading-10 tracking-tight text-gray-400">25 Nov</dd>
-                                    </div>
-                                    <div>
-                                        <dt className="text-sm font-semibold leading-6 text-gray-300">Date</dt>
-                                        <dd className="mt-2 text-3xl font-bold leading-10 tracking-tight text-gray-400">
-                                            {data[0].start_date}
-                                        </dd>
-                                    </div>
-                                    <div>
-                                        <dt className="text-sm font-semibold leading-6 text-gray-300">Fee</dt>
-                                        <dd className="mt-2 text-3xl font-bold leading-10 tracking-tight text-gray-400">
+                                <div className="mt-8 lg:col-span-2 col-span-1 border-t border-gray-300/10 pt-10 sm:col-span-4 font-satoshi">
+                                    <div className="grid grid-cols-3 gap-x-4 gap-y-2">
+                                        <div className=" text-2xl font-semibold leading-6 text-gray-200">
+                                            <dt>Last Date</dt>
+                                        </div>
+                                        <div className="text-2xl font-semibold leading-6 text-gray-200">
+                                            <dt>Date</dt>
+                                        </div>
+                                        <div className="text-2xl font-semibold leading-6 text-gray-200">
+                                            <dt>Fee</dt>
+                                        </div>
+                                        <div className="mt-2 text-3xl font-bold leading-10 tracking-tight text-gray-400">
+                                            {formatDate(data[0].ticket_end_date)}
+                                        </div>
+                                        <div className="mt-2 text-3xl font-bold leading-10 tracking-tight text-gray-400">
+                                            {formatDate(data[0].start_date)}
+                                        </div>
+                                        <div className="mt-2 text-3xl font-bold leading-10 tracking-tight text-gray-400">
                                             {data[0].price}
-                                        </dd>
-                                    </div>
-                                    <div>
-                                        <dt className="text-sm font-semibold leading-6 text-gray-300">Venue</dt>
-                                        <dd className="mt-2 text-3xl font-bold leading-10 tracking-tight text-gray-400">
+                                        </div>
+                                        <div className=" font-semibold text-2xl self-center leading-6 text-gray-200">
+                                            <dt >Venue</dt>
+                                        </div>
+                                        <div className="mt-2 text-3xl col-span-2 font-bold leading-10 tracking-tight text-gray-400">
                                             {data[0].venue}
-                                        </dd>
+                                        </div>
                                     </div>
-                                </dl>
+                                </div>
+
                             </div>
                         </div>
                     )
@@ -130,3 +135,4 @@ function Page() {
 }
 
 export default Page;
+
