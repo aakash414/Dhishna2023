fc6b35bb57bc54d59859de12c200a471d873a2cd
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-11-09 01:12:09 +0530

    event text animation

--- src/components/Slide7.jsx
+++ src/components/Slide7.jsx
@@ -25,7 +25,7 @@ export function Slide7() {
             setIsScrolling(true)
             setTimeout(() => {
                 setIsScrolling(false)
-            }, 100)
+            }, 300)
         };
 
         document.addEventListener('scroll', handleScroll);
