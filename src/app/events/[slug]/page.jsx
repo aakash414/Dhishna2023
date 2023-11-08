00be36df8ac66cad3e5a3c01295fdfbce45cd7b2
aakash <aakashpr975@gmail.com>
2023-11-08 19:39:14 +0530

    data fetch success

--- src/app/events/[slug]/page.jsx
+++ src/app/events/[slug]/page.jsx
@@ -1,87 +1,127 @@
-import React from 'react'
-import miniDebConfLogo from '../../../images/miniDebConf.png'
-import Image from 'next/image'
-import client from '../../../client'
+"use client"
+import client from '@/client';
+import React, { useEffect, useState } from 'react';
+import { imageUrlBuilder } from '@sanity/image-url';
+import myConfiguredSanityClient from '@/sanityClient';
 
 
+function Page() {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const builder = imageUrlBuilder(myConfiguredSanityClient)
 
-export async function getServerSideProps(context) {
-    // Fetch event data from Sanity based on the dynamic slug
-    const { slug } = context.params;
-
-    const query = `*[_type == "event" && slug.current == $slug][0] {
-    imageLink,
-    heading,
-    title,
-    para1,
-    para2,
-    para3,
-    date,
-    fee,
-    venue
-  }`;
-
-    const eventData = await client.fetch(query, { slug });
-
-    if (!eventData) {
-        return {
-            notFound: true, // Handle not found events
-        };
+    function urlFor(source) {
+        return builder.image(source)
     }
 
-    return {
-        props: { eventData },
-    };
-}
 
-function page({eventData}) {
 
+    useEffect(() => {
+        // Fetch the data from Sanity when the component mounts
+        client
+            .fetch(
+                `*[_type == "workshop"]{
+          title,
+          event_code,
+          icon,
+          details,
+          start_date,
+          price,
+          venue
+        }`
+            )
+            .then((eventData) => {
+
+                const data = eventData.filter((event) => event.event_code == slug);
+                setData(data);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.error("Error fetching data:", error);
+                setLoading(false);
+            });
+    }, []);
+
+    const slug = window.location.href.split('/').pop();
+    console.log("slug",slug);
+
+    console.log("data",data)
+
+    useEffect(() => {
+        // Fetch data of the event using the slug
+        client
+            .fetch(
+                `*[_type == "workshop" && event_code == "${slug}"]{
+          title,
+          event_code,
+          icon,
+          details,
+          start_date,
+          price,
+          venue
+        }`
+            )
+            .then((data) => {
+                setData(data);
+                console.log("Received filtered data:", data)
+            })
+            .catch(console.error);
+    }, [slug]);
 
     return (
-        <div className=" py-24 sm:py-32">
-            <div className=" px-6 lg:px-8">
-
-                    <div className="mx-10 flex lg:flex-row flex-col justify-center items-start gap-x-8 gap-y-16 sm:gap-y-24 lg:mx-0 lg:max-w-none" >
-                        <div className=" overflow-hidden h-fit w-auto rounded-3xl bg-gray-900  shadow-2xl">
-                            <Image
-                                className="  object-cover "
-                                src={miniDebConfLogo}
-                                alt=""
-                            />
-                        </div>
-                        <div>
-                            <div className="text-base leading-7 text-gray-300 lg:max-w-lg">
-                                <p className="text-base font-satoshi font-semibold leading-7 text-orange-600">{eventData.heading}</p>
-                                <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-300 sm:text-4xl">{eventData.title}</h1>
-                                <div className="max-w-xl">
-                                    <p className="mt-6">{eventData.para1} </p>
-                                    <p className="mt-8">{eventData.para2}</p>
-                                    <p className="mt-8">{eventData.para3}</p>
-                                </div>
+        <div className="py-24 sm:py-32">
+            <div className="px-6 lg:px-8">
+                {loading ? (
+                    <p>Loading...</p>
+                ) : (
+                    data && (
+                        <div className="mx-10 flex lg:flex-row flex-col justify-center items-start gap-x-8 gap-y-16 sm:gap-y-24 lg:mx-0 lg:max-w-none">
+                            <div className="overflow-hidden h-fit w-auto rounded-3xl bg-gray-900 shadow-2xl">
+                                <img className="object-cover" src={data[0].icon.asset.ref} alt="" />
                             </div>
-                            <dl className="mt-8 grid grid-cols-2 gap-8 border-t border-gray-300/10 pt-10 sm:grid-cols-4 font-satoshi">
-                                <div>
-                                    <dt className="text-sm font-semibold leading-6 text-gray-300">Last Date</dt>
-                                    <dd className="mt-2 text-3xl font-bold leading-10 tracking-tight text-gray-400">25 Nov</dd>
+                            <div>
+                                <div className="text-base leading-7 text-gray-300 lg:max-w-lg">
+                                    <p className="text-base font-satoshi font-semibold leading-7 text-orange-600">
+                                        {data[0].title}
+                                    </p>
+                                    <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-300 sm:text-4xl">
+                                        {data[0].title}
+                                    </h1>
+                                    <div className="max-w-xl">
+                                        <p className="mt-6">{data[0].details}</p>
+                                    </div>
                                 </div>
-                                <div>
-                                    <dt className="text-sm font-semibold leading-6 text-gray-300">Date</dt>
-                                    <dd className="mt-2 text-3xl font-bold leading-10 tracking-tight text-gray-400">eventData.date</dd>
-                                </div>
-                                <div>
-                                    <dt className="text-sm font-semibold leading-6 text-gray-300">Fee</dt>
-                                    <dd className="mt-2 text-3xl font-bold leading-10 tracking-tight text-gray-400">{eventData.fee}</dd>
-                                </div>
-                                <div>
-                                    <dt className="text-sm font-semibold leading-6 text-gray-300">Venue</dt>
-                                    <dd className="mt-2 text-3xl font-bold leading-10 tracking-tight text-gray-400">{eventData.venue}</dd>
-                                </div>
-                            </dl>
+                                <dl className="mt-8 grid grid-cols-2 gap-8 border-t border-gray-300/10 pt-10 sm:grid-cols-4 font-satoshi">
+                                    <div>
+                                        <dt className="text-sm font-semibold leading-6 text-gray-300">Last Date</dt>
+                                        <dd className="mt-2 text-3xl font-bold leading-10 tracking-tight text-gray-400">25 Nov</dd>
+                                    </div>
+                                    <div>
+                                        <dt className="text-sm font-semibold leading-6 text-gray-300">Date</dt>
+                                        <dd className="mt-2 text-3xl font-bold leading-10 tracking-tight text-gray-400">
+                                            {data[0].start_date}
+                                        </dd>
+                                    </div>
+                                    <div>
+                                        <dt className="text-sm font-semibold leading-6 text-gray-300">Fee</dt>
+                                        <dd className="mt-2 text-3xl font-bold leading-10 tracking-tight text-gray-400">
+                                            {data.price}
+                                        </dd>
+                                    </div>
+                                    <div>
+                                        <dt className="text-sm font-semibold leading-6 text-gray-300">Venue</dt>
+                                        <dd className="mt-2 text-3xl font-bold leading-10 tracking-tight text-gray-400">
+                                            {data.venue}
+                                        </dd>
+                                    </div>
+                                </dl>
+                            </div>
                         </div>
-                    </div>
+                    )
+                )}
             </div>
         </div>
-    )
+    );
 }
 
-export default page;
+export default Page;
--- src/app/workshop/[slug]/page.jsx
+++ src/app/workshop/[slug]/page.jsx
@@ -1,91 +1,131 @@
-import React from 'react'
-import miniDebConfLogo from '../../../images/miniDebConf.png'
-import Image from 'next/image'
-// import { useRouter } from 'next/navigation'
+"use client"
+import client from '@/client';
+import React, { useEffect, useState } from 'react';
 
-//store the data of each events such as image link, heading, title, description, date, time, venue as object
 
-function page() {
-    // //fetch the id which is the slug of the event
-    // const router = useRouter()
-    // const { slug } = router.query
+function Page() {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-    const eventsData = [
-        {
-            imageLink: miniDebConfLogo,
-            heading: "Company values",
-            title: "On a mission to empower remote teams",
-            para1: "Faucibus commodo massa rhoncus, volutpat. Dignissim sed egetrisus enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit.  Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id",
-            para2: "Faucibus commodo massa rhoncus, volutpat. Dignissim sed egetrisus enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit.  Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id",
-            para3: "Faucibus commodo massa rhoncus, volutpat. Dignissim sed egetrisus enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit.  Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id",
-            date: "2023-11-15",
-            time: "10:00 AM - 2:00 PM",
-            venue: "Seminar Hall",
+    useEffect(() => {
+        // Fetch the data from Sanity when the component mounts
+        client
+            .fetch(
+                `*[_type == "workshop"]{
+          title,
+          event_code,
+          icon,
+          details,
+          start_date,
+          price,
+          venue
+        }`
+            )
+            .then((eventData) => {
+                console.log("Received data:", eventData);
+            // Filter the data to find the item with a matching event_code
+            const filteredData = eventData.filter((event) => event.event_code == slug);
 
-        }
-    ]
+            if (filteredData.length > 0) {
+                setData(filteredData);
+                console.log("Received filtered data:", filteredData);
+                setLoading(false);
+            } else {
+                setLoading(false);
+                console.error("Data not found for slug:", slug);
+            }
+        })
+            .catch((error) => {
+                console.error("Error fetching data:", error);
+                setLoading(false);
+            });
+    }, []);
+
+    const slug = window.location.href.split('/').pop();
+    console.log("slug",slug);
+
+    console.log("data",data)
+
+    // useEffect(() => {
+    //     // Fetch data of the event using the slug
+    //     client
+    //         .fetch(
+    //             `*[_type == "workshop" && event_code == "${slug}"]{
+    //       title,
+    //       event_code,
+    //       icon,
+    //       details,
+    //       start_date,
+    //       price,
+    //       venue
+    //     }`
+    //         )
+    //         .then((data) => {
+    //             setData(data);
+    //             console.log("Received filtered data:", data)
+    //         })
+    //         .catch(console.error);
+    // }, [slug]);
 
     return (
-        <div className=" py-24 sm:py-32">
-            <div className=" px-6 lg:px-8">
+        <div className="py-24 sm:py-32">
+            <div className="px-6 lg:px-8">
+                {loading ? (
+                    <p>Loading...</p>
+                ) : (
+                    data && (
+                        <div className="mx-10 flex lg:flex-row flex-col justify-center items-start gap-x-8 gap-y-16 sm:gap-y-24 lg:mx-0 lg:max-w-none">
+                            <div className="overflow-hidden h-fit w-auto rounded-3xl bg-gray-900 shadow-2xl">
+                                <img className="object-cover" src={data[0].icon} alt="" />
+                            </div>
+                            <div>
+                                <div className="text-base leading-7 text-gray-300 lg:max-w-lg">
+                                    <p className="text-base font-satoshi font-semibold leading-7 text-orange-600">
+                                        {data[0].title}
+                                    </p>
+                                    <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-300 sm:text-4xl">
+                                        {data[0].title}
+                                    </h1>
+                                    <div className="max-w-xl">
+                                        <p className="mt-6">{data[0].details}</p>
+                                    </div>
+                                </div>
+                                <dl className="mt-8 grid grid-cols-2 gap-8 border-t border-gray-300/10 pt-10 sm:grid-cols-4 font-satoshi">
+                                    <div>
+                                        <dt className="text-sm font-semibold leading-6 text-gray-300">Last Date</dt>
+                                        <dd className="mt-2 text-3xl font-bold leading-10 tracking-tight text-gray-400">25 Nov</dd>
+                                    </div>
+                                    <div>
+                                        <dt className="text-sm font-semibold leading-6 text-gray-300">Date</dt>
+                                        <dd className="mt-2 text-3xl font-bold leading-10 tracking-tight text-gray-400">
+                                            {data[0].start_date}
+                                        </dd>
+                                    </div>
+                                    <div>
+                                        <dt className="text-sm font-semibold leading-6 text-gray-300">Fee</dt>
+                                        <dd className="mt-2 text-3xl font-bold leading-10 tracking-tight text-gray-400">
+                                            {data.price}
+                                        </dd>
+                                    </div>
+                                    <div>
+                                        <dt className="text-sm font-semibold leading-6 text-gray-300">Venue</dt>
+                                        <dd className="mt-2 text-3xl font-bold leading-10 tracking-tight text-gray-400">
+                                            {data.venue}
+                                        </dd>
+                                    </div>
+                                </dl>
+                                <button type="button" className="text-orange-600 bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-semibold rounded-lg text-2xl font-satoshi px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#050708]/50 dark:hover:bg-[#050708]/30 mr-2 mb-2">
+                                    Register Here
+                                </button>
 
-                <div className="mx-10 flex lg:flex-row flex-col justify-center items-start gap-x-8 gap-y-16 sm:gap-y-24 lg:mx-0 lg:max-w-none" >
-                    <div className=" overflow-hidden h-fit w-auto rounded-3xl bg-gray-900  shadow-2xl">
-                        <Image
-                            className="  object-cover "
-                            src={miniDebConfLogo}
-                            alt=""
-                        />
-                    </div>
-                    <div>
-                        <div className="text-base leading-7 text-gray-300 lg:max-w-lg">
-                            <p className="text-base font-satoshi font-semibold leading-7 text-orange-600">MiniDebConf</p>
-                            <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-300 sm:text-4xl">On a
-                                mission to empower remote teams</h1>
-                            <div className="max-w-xl">
-                                <p className="mt-6">Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis
-                                    odio id et. Id blandit molestie auctor fermentum dignissim. Lacus diam tincidunt
-                                    ac cursus in vel. Mauris varius vulputate et ultrices hac adipiscing egestas.
-                                    Iaculis convallis ac tempor et ut. Ac lorem vel integer orci.   </p>
-                                <p className="mt-8">Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis
-                                    odio id et. Id blandit molestie auctor fermentum dignissim. Lacus diam tincidunt
-                                    ac cursus in vel. Mauris varius vulputate et ultrices hac adipiscing egestas.
-                                    Iaculis convallis ac tempor et ut. Ac lorem vel integer orci.</p>
-                                <p className="mt-8">Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis
-                                    odio id et. Id blandit molestie auctor fermentum dignissim. Lacus diam tincidunt
-                                    ac cursus in vel. Mauris varius vulputate et ultrices hac adipiscing egestas.
-                                    Iaculis convallis ac tempor et ut. Ac lorem vel integer orci.</p>
                             </div>
                         </div>
-                        <dl className="mt-8 grid grid-cols-2 gap-8 border-t border-gray-300/10 pt-10 sm:grid-cols-4 font-satoshi">
-                            <div>
-                                <dt className="text-sm font-semibold leading-6 text-gray-300">Last Date</dt>
-                                <dd className="mt-2 text-3xl font-bold leading-10 tracking-tight text-gray-400">25 Nov</dd>
-                            </div>
-                            <div>
-                                <dt className="text-sm font-semibold leading-6 text-gray-300">Date</dt>
-                                <dd className="mt-2 text-3xl font-bold leading-10 tracking-tight text-gray-400">25 Nov</dd>
-                            </div>
-                            <div>
-                                <dt className="text-sm font-semibold leading-6 text-gray-300">Fee</dt>
-                                <dd className="mt-2 text-3xl font-bold leading-10 tracking-tight text-gray-400">499</dd>
-                            </div>
-                            <div>
-                                <dt className="text-sm font-semibold leading-6 text-gray-300">Venue</dt>
-                                <dd className="mt-2 text-3xl font-bold leading-10 tracking-tight text-gray-400">Old Soe</dd>
-                            </div>
-                        </dl>
-                        <div className="mt-7 flex md:justify-start justify-center ">
-                            <button type="button" className="text-orange-600 bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-semibold rounded-lg text-2xl font-satoshi px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#050708]/50 dark:hover:bg-[#050708]/30 mr-2 mb-2">
-                                Register Here
-                            </button>
-
-                        </div>
-                    </div>
-                </div>
+                    )
+                )}
             </div>
         </div>
-    )
+    );
 }
 
-export default page;
+export default Page;
+
